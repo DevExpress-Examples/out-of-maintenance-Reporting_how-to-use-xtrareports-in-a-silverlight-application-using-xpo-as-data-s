ff124cd987c0634623b2bbadcd55e9c62f635e18
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Q279518.Web/XpoHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Northwind;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+
+/// <summary>
+/// Summary description for XpoHelper
+/// </summary>
+public static class XpoHelper {
+    public static Session GetNewSession() {
+        return new Session(DataLayer);
+    }
+
+    public static UnitOfWork GetNewUnitOfWork() {
+        return new UnitOfWork(DataLayer);
+    }
+
+    private readonly static object lockObject = new object();
+
+    static volatile IDataLayer fDataLayer;
+    static IDataLayer DataLayer {
+        get {
+            if (fDataLayer == null) {
+                lock (lockObject) {
+                    if (fDataLayer == null) {
+                        fDataLayer = GetDataLayer();
+                    }
+                }
+            }
+            return fDataLayer;
+        }
+    }
+
+    private static IDataLayer GetDataLayer() {
+        XpoDefault.Session = null;
+        XPDictionary dict = new ReflectionDictionary();
+        dict.GetDataStoreSchema(typeof(Category).Assembly);
+        return new ThreadSafeDataLayer(dict, 
+            XpoDefault.GetConnectionProvider(MSSqlConnectionProvider.GetConnectionString("(local)", "Northwind"),
+            AutoCreateOption.None));
+    }
+}
